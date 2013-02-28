60d0ef7542c3d2d22b176836405cf07ee6690418
timwhitlock <timwhitlock@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-28 13:55:23 +0000

    pushed 1.0.3 to stable
    git-svn-id: https://plugins.svn.wordpress.org/latest-tweets-widget/tags/1.0.3@674576 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ latest-tweets.php
@@ -0,0 +1,187 @@
+<?php
+/*
+Plugin Name: Latest Tweets
+Plugin URI: http://wordpress.org/extend/plugins/latest-tweets-widget/
+Description: Provides a sidebar widget showing latest tweets - compatible with the new Twitter API 1.1
+Author: Tim Whitlock
+Version: 1.0.3
+Author URI: http://timwhitlock.info/
+*/
+
+
+
+/**
+ * Pull latest tweets with some caching of raw data.
+ * @param string account whose tweets we're pulling
+ * @param int number of tweets to get and display
+ * @param bool whether to show retweets
+ * @param bool whether to show at replies
+ * @return array blocks of html expected by the widget
+ */
+function latest_tweets_render( $screen_name, $count, $rts, $ats ){
+    try {
+        if( ! function_exists('twitter_api_get') ){
+            require_once dirname(__FILE__).'/lib/twitter-api.php';
+        }
+        if( function_exists('apc_fetch') ){
+            // We could cache the rendered HTML right here, but this keeps caching abstracted in library
+            twitter_api_enable_cache( 300 );
+        }
+        // Build API params for "statuses/user_timeline" // https://dev.twitter.com/docs/api/1.1/get/statuses/user_timeline
+        $trim_user = true;
+        $include_rts = ! empty($rts);
+        $exclude_replies = empty($ats);
+        $params = compact('count','exclude_replies','include_rts','trim_user','screen_name');
+        if( $exclude_replies || ! $include_rts ){
+            // Stripping tweets means we may get less than $count tweets.
+            // there is no good way around this other than fetch extra and hope for the best
+            $params['count'] *= 3;
+        }
+        $tweets = twitter_api_get('statuses/user_timeline', $params );
+        if( isset($tweets[$count]) ){
+            $tweets = array_slice( $tweets, 0, $count );
+        }
+        // render each tweet as a blocks of html for the widget list items
+        $rendered = array();
+        foreach( $tweets as $tweet ){
+            extract( $tweet );
+            $link = esc_html( 'http://twitter.com/'.$screen_name.'/status/'.$id_str);
+            // render nice datetime, unless theme overrides with filter
+            $date = apply_filters( 'latest_tweets_render_date', $created_at );
+            if( $date === $created_at ){
+                function_exists('twitter_api_relative_date') or twitter_api_include('utils');
+                $date = esc_html( twitter_api_relative_date($created_at) );
+                $date = '<time datetime="'.$created_at.'">'.$date.'</time>';
+            }
+            // render and linkify tweet, unless theme overrides with filter
+            $html = apply_filters('latest_tweets_render_text', $text );
+            if( $html === $text ){
+                function_exists('twitter_api_html') or twitter_api_include('utils');
+                $html = twitter_api_html( $text );
+            }
+            // piece together the whole tweet, allowing overide
+            $final = apply_filters('latest_tweets_render_tweet', $html, $date, $link );
+            if( $final === $html ){
+                $final = '<p class="tweet-text">'.$html.'</p>'.
+                         '<p class="tweet-details"><a href="'.$link.'" target="_blank">'.$date.'</a></p>';
+            }
+            $rendered[] = $final;
+        }
+        return $rendered;
+    }
+    catch( Exception $Ex ){
+        return array( '<p class="tweet-text"><strong>Error:</strong> '.esc_html($Ex->getMessage()).'</p>' );
+    }
+} 
+
+
+ 
+  
+/**
+ * Example latest tweets widget class
+ */
+class Latest_Tweets_Widget extends WP_Widget {
+    
+    /** @see WP_Widget::__construct */
+    public function __construct( $id_base = false, $name = 'Latest Tweets', $widget_options = array(), $control_options = array() ){
+        $this->options = array(
+            array (
+                'name'  => 'title',
+                'label' => __('Widget title'),
+                'type'  => 'text'
+            ),
+            array (
+                'name'  => 'screen_name',
+                'label' => __('Twitter handle'),
+                'type'  => 'text'
+            ),
+            array (
+                'name'  => 'num',
+                'label' => __('Number of tweets'),
+                'type'  => 'text'
+            ),
+            array (
+                'name'  => 'rts',
+                'label' => __('Show Retweets'),
+                'type'  => 'bool'
+            ),
+            array (
+                'name'  => 'ats',
+                'label' => __('Show Replies'),
+                'type'  => 'bool'
+            ),
+        );
+        parent::__construct( $id_base, $name, $widget_options, $control_options );  
+    }    
+    
+    /* ensure no missing keys in instance params */
+    private function check_instance( $instance ){
+        if( ! is_array($instance) ){
+            $instance = array();
+        }
+        $instance += array (
+            'title' => __('Latest Tweets'),
+            'screen_name' => '',
+            'num' => '5',
+            'rts' => '',
+            'ats' => '',
+        );
+        return $instance;
+    }
+    
+    /** @see WP_Widget::form */
+    public function form( $instance ) {
+        $instance = $this->check_instance( $instance );
+        foreach ( $this->options as $val ) {
+            $elmid = $this->get_field_id( $val['name'] );
+            $fname = $this->get_field_name($val['name']);
+            $value = isset($instance[ $val['name'] ]) ? $instance[ $val['name'] ] : '';
+            $label = '<label for="'.$elmid.'">'.$val['label'].'</label>';
+            if( 'bool' === $val['type'] ){
+                 $checked = $value ? ' checked="checked"' : '';
+                 echo '<p><input type="checkbox" value="1" id="'.$elmid.'" name="'.$fname.'"'.$checked.' /> '.$label.'</p>';
+            }
+            else {
+                $attrs = '';
+                echo '<p>'.$label.'<br /><input class="widefat" type="text" value="'.esc_attr($value).'" id="'.$elmid.'" name="'.$fname.'" /></p>';
+            }
+        }
+    }
+
+    /** @see WP_Widget::widget */
+    public function widget( array $args, $instance ) {
+        extract( $this->check_instance($instance) );
+        // title is themed via Wordpress widget theming techniques
+        $title = $args['before_title'] . apply_filters('widget_title', $title) . $args['after_title'];
+        // by default tweets are rendered as an unordered list
+        $items = latest_tweets_render( $screen_name, $num, $rts, $ats );
+        $list  = apply_filters('latest_tweets_render_list', $items );
+        if( is_array($list) ){
+            $list = '<ul><li>'.implode('</li><li>',$items).'</li></ul>';
+        }
+        // output widget applying filters to each element
+        echo 
+        $args['before_widget'], 
+            '<div class="latest-tweets">', 
+                $title,
+                apply_filters( 'latest_tweets_render_before', '' ),
+                $list,
+                apply_filters( 'latest_tweets_render_after', '' ),
+            '</div>',
+         $args['after_widget'];
+    }
+    
+}
+ 
+
+
+function latest_tweets_register_widget(){
+    return register_widget('Latest_Tweets_Widget');
+}
+
+add_action( 'widgets_init', 'latest_tweets_register_widget' );
+
+
+if( is_admin() ){
+    require_once dirname(__FILE__).'/lib/twitter-api.php';
+}
---
+++ readme.txt
@@ -0,0 +1,145 @@
+=== Plugin Name ===
+Contributors: timwhitlock
+Donate link: http://timwhitlock.info/donate-to-a-project/
+Tags: twitter, tweets, oauth, api, rest, api, widget, sidebar
+Requires at least: 3.5.1
+Tested up to: 3.5.1
+Stable tag: 1.0.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Latest Tweets widget compatible with the new Twitter API 1.1
+
+== Description ==
+
+Connect your Twitter account to this plugin and the widget will display your latest tweets on your site.
+
+This plugin is compatible with the new **Twitter API 1.1** and provides full **OAuth** authentication via the Wordpress admin area.
+ 
+
+Built by [timwhitlock](https://twitter.com/timwhitlock)
+
+The underlying Twitter API library is [available on Github](https://github.com/timwhitlock/wp-twitter-api)
+
+
+== Installation ==
+
+1. Unzip all files to the `/wp-content/plugins/` directory
+2. Log into Wordpress admin and activate the 'Latest Tweets' plugin through the 'Plugins' menu
+
+Once the plugin is installed and enabled you can bind it to a Twitter account as follows:
+
+3. Register a Twitter application at https://dev.twitter.com/apps
+4. Note the Consumer key and Consumer secret under OAuth settings
+5. Log into Wordpress admin and go to Settings > Twitter API
+6. Enter the consumer key and secret and click 'Save settings'
+7. Click the 'Connect to Twitter' button and follow the prompts.
+
+Once your site is authenticated you can configure the widget as follows:
+
+8. Log into Wordpress admin and go to Appearance > Widgets
+9. Drag 'Latest Tweets' from 'Available widgets' to where you want it. e.g. Main Sidebar
+10. Optionally configure the widget title and number of tweets to display.
+
+== Frequently Asked Questions ==
+
+= How can I style the widget? =
+
+See the 'Other Notes' tab for theming information.
+
+= How I do know what my OAuth settings are? =
+
+These details are available in the [Twitter dashboard](https://dev.twitter.com/apps)
+
+= What do I put in the third and fourth fields? =
+
+Once you've populated the first two fields, just click the *Connect* button and follow the prompts.
+
+
+
+== Changelog ==
+
+= 1.0.3 =
+* Added theme filters
+* Added configs for showing replies and RTs
+
+= 1.0.2 =
+* Fixed hook for PHP < 5.3
+
+= 1.0.1 =
+* First public release
+
+== Theming ==
+
+For starters you can alter some of the HTML using built-in WordPress features.  
+See [Widget Filters](http://codex.wordpress.org/Plugin_API/Filter_Reference#Widgets)
+and [Widgetizing Themes](http://codex.wordpress.org/Widgetizing_Themes)
+
+**CSS**
+
+This plugin contains no default CSS. That's deliberate, so you can style it how you want.
+
+Tweets are rendered as a list which has various hooks you can use. Here's a rough template:
+
+    .latest-tweets {
+        /* style tweet list wrapper */
+    }
+    .latest-tweets h3 {
+        /* style whatever you did with the header */
+    }
+    .latest-tweets ul { 
+        /* style tweet list*/
+    }
+    .latest-tweets li {
+       /* style tweet item */
+    }
+    .latest-tweets .tweet-text {
+       /* style main tweet text */
+    }
+    .latest-tweets .tweet-text a {
+       /* style links, hashtags and mentions */
+    }
+    .latest-tweets .tweet-details {
+      /* style datetime and link under tweet */
+    }
+
+
+**Custom HTML**
+
+If you want to override the default markup of the tweets, the following filters are also available:
+
+* Add a header between the widget title and the tweets with `latest_tweets_render_before`
+* Perform your own rendering of the timestamp with `latest_tweets_render_date`
+* Render plain tweet text to your own HTML with `latest_tweets_render_text`
+* Render each composite tweet with `latest_tweets_render_tweet`
+* Override the unordered list for tweets with `latest_tweets_render_list` 
+* Add a footer before the end of the widget with `latest_tweets_render_after`
+
+Here's an **example** of using some of the above in tour theme's functions.php file:
+
+    add_filter('latest_tweets_render_date', function( $created_at ){
+        $date = DateTime::createFromFormat('D M d H:i:s O Y', $created_at );
+        return $date->format('d M h:ia');
+    }, 10 , 1 );
+    
+    add_filter('latest_tweets_render_text', function( $text ){
+        return $text; // <- will use default
+    }, 10 , 1 );
+    
+    add_filter('latest_tweets_render_tweet', function( $html, $date, $link ){
+        return '<p class="my-tweet">'.$html.'</p><p class="my-date"><a href="'.$link.'">'.$date.'</a></p>';
+    }, 10, 3 );
+    
+    add_filter('latest_tweets_render_after', function(){
+        return '<footer><a href="https://twitter.com/me">More from me</a></footer>';
+    }, 10, 0 );
+
+
+== Credits ==
+
+Screenshot taken with permission from http://stayingalivefoundation.org/blog
+
+== Notes ==
+
+Be aware of [Twitter's display requirements](https://dev.twitter.com/terms/display-requirements) when rendering tweets on your website.
+
